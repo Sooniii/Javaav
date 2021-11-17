c2071a990512adb8d19450632acb9a82df826ff1
Sooniii <theodulejanchen@hotmail.fr>
2021-11-17 09:36:09 +0100

    first commit

---
+++ src/main/java/groupe3/Javaav/JavaavApplication.java
@@ -0,0 +1,13 @@
+package groupe3.Javaav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaavApplication.class, args);
+	}
+
+}
