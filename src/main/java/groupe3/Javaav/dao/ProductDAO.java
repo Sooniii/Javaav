b38faa50725aa869805b6e2c4724472afd15c842
Alexis Rabiller <admin@MacBook-Pro-de-Alexis.local>
2021-11-18 10:06:31 +0100

    delete ProductDAO finish

--- src/main/java/groupe3/Javaav/dao/ProductDAO.java
+++ src/main/java/groupe3/Javaav/dao/ProductDAO.java
@@ -26,5 +26,10 @@ public class ProductDAO {
         return jdbcTemplate.update(sql, p.getName(), p.getType(), p.getRating());
     }
 
+    public String delete(Long categoryId){
+        String sql = "DELETE FROM products WHERE id=?";
+        jdbcTemplate.update(sql, categoryId);
+        return "DELETE FROM products WHERE id=" + categoryId;
+    }
 
 }
